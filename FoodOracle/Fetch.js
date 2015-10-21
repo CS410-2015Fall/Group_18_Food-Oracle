c6cebf90aff1dfff185f86368180a301058c46dd
Randy Hsu <randyhsu2010@gmail.com>
2015-10-20 19:43:48 -0700

    Move more code over to fetch

--- FoodOracle/Fetch.js
+++ FoodOracle/Fetch.js
@@ -1,9 +1,17 @@
+var appID = "a05ca702";
+var appKey = "7ce644115e75fa844afb42c6079fc6c8";
+var baseURL = "http://api.yummly.com/v1/api/recipes?";
+
 function Fetch(parentContext) {
+	var subURL = '_app_id=' + appID + '&_app_key=' + appKey;
 	this.parentContext = parentContext;
-	this.fetchRequest = function(URL, callback){
-		fetch(URL)
+	this.fetchRequest = function(request, callback){
+		var fetchURL = baseURL + subURL + '&q=' + request;
+		debugger;
+		fetch(fetchURL)
 		.then((response) => response.json())
 		.then((responseData) => {
+			debugger;
 			callback(parentContext, responseData);
 		}).done();
 	}
