2b043f78f3677429ee0ae5e79ce76e7fa8e8e857
Randy Hsu <randyhsu2010@gmail.com>
2015-10-20 19:53:30 -0700

    Fix a bug

--- FoodOracle/Fetch.js
+++ FoodOracle/Fetch.js
@@ -5,13 +5,11 @@ var baseURL = "http://api.yummly.com/v1/api/recipes?";
 function Fetch(parentContext) {
 	var subURL = '_app_id=' + appID + '&_app_key=' + appKey;
 	this.parentContext = parentContext;
-	this.fetchRequest = function(request, callback){
+	this.searchRequest = function(request, callback){
 		var fetchURL = baseURL + subURL + '&q=' + request;
-		debugger;
 		fetch(fetchURL)
 		.then((response) => response.json())
 		.then((responseData) => {
-			debugger;
 			callback(parentContext, responseData);
 		}).done();
 	}
